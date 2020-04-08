a380e0aa13106d69dd3f374174fc34a23df2cc12
Vadim Klimchuk <vadimklimchuk77@gmail.com>
2020-04-08 17:03:22 +0300

    refactor counter component

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 
 import { Store, select } from '@ngrx/store';
-import { Observable, interval, Subscription } from 'rxjs';
+import { Observable, Subscription, interval } from 'rxjs';
 
 import { BaseClass } from 'src/app/shared/base/base.class';
 import { CountState } from 'src/app/store/reducers/counter.reducer';
-import { ChangeAction, ResetAction } from 'src/app/store/actions/counter.actions';
+import { ResetAction, ChangeAction } from 'src/app/store/actions/counter.actions';
 import { selectFirstValue, selectSecondValue } from 'src/app/store/selector/counter.selector';
+import { tap } from 'rxjs/operators';
 
+
+enum CounterState {
+  STARTED = 'started',
+  STOPPED = 'stopped',
+}
 
 @Component({
   selector: 'app-counter',
@@ -18,34 +24,38 @@ export class CounterComponent extends BaseClass {
   public firstValue$: Observable<number> = this.store$.pipe(select(selectFirstValue));
   public secondValue$: Observable<number> = this.store$.pipe(select(selectSecondValue));
 
-  public isEnabledStart: boolean = true;
+  public currentState: CounterState = CounterState.STOPPED;
 
   public intervalSubscription: Subscription;
-
-  public count$: Observable<number>;
 
   constructor(private store$: Store<CountState>) { super(); }
 
   public handleStartClick(): void {
+    if (this.currentState === CounterState.STARTED) {
+      return;
+    }
+
     this.intervalSubscription = interval(1000)
+      .pipe(
+        tap(() => this.currentState = CounterState.STARTED)
+      )
       .subscribe(() => {
         this.store$.dispatch(new ChangeAction());
-
-        this.isEnabledStart = false;
       });
 
     this.subscribe(this.intervalSubscription);
   }
 
   public handleStopClick(): void {
-    this.isEnabledStart = true;
     this.unsubscribe();
+
+    this.currentState = CounterState.STOPPED;
   }
 
   public handleResetClick(): void {
-    this.isEnabledStart = true;
     this.unsubscribe();
 
+    this.currentState = CounterState.STOPPED;
     this.store$.dispatch(new ResetAction());
   }
 }
